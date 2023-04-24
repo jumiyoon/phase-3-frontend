d37a25402d4931b357dcdf1da73845cfecc27d98
Jumi Yoon <jums.code@gmail.com>
2023-04-24 12:48:17 -0700

    create search bar

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,25 @@
 import React from "react";
 import { Input } from 'semantic-ui-react'
 
-function Search() {
+function Search({search, setSearch}) {
 
-
+    return (
+        <div>
+            <br />
+            <Input
+                size="large"
+                icon='search'
+                type="search"
+                name="search"
+                placeholder="Search name..."
+                onChange={handleChange}
+                value={search}
+                autoComplete="off"
+                />
+                <br />
+                <br/>
+        </div>
+    )
 }
 
 export default Search;
