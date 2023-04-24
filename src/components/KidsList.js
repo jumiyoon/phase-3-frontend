ad93f0a504ae539bb14467bb22d7f1ccfa36f23b
Jumi Yoon <jums.code@gmail.com>
2023-04-24 12:31:57 -0700

    create KidInfo component

--- src/components/KidsList.js
+++ src/components/KidsList.js
@@ -1,8 +1,21 @@
 import React from "react";
+import KidInfo from "./KidInfo";
 
-function KidsList() {
+function KidsList( {kids} ) {
     return (
         <div>
+            {kids.map((kid) => (
+            <KidInfo
+                key={kid.id}
+                name={kid.name}
+                id={kid.id}
+                dietaryRestrictions={kid.dietary_restrictions}
+                // parentName={kid.parent.name}
+                // parentPhone={kid.parent.phone}
+                // pickupTime={kid.parent.service_time}
+                // onDeleteKid={onDeleteKid}
+            />
+            ))}
         </div>
     )
 }
