21f0ae45b3af390e05d3097c1b24b81a64b31a50
Jumi Yoon <jums.code@gmail.com>
2023-04-24 13:06:08 -0700

    add newkid form modal

--- src/components/NewKid.js
+++ src/components/NewKid.js
@@ -1,7 +1,43 @@
 import React, { useState } from "react";
 
 function NewKid(){
+const [formData, setFormData] = useState({
+    name: "",
+    dietary_restrictions: ""
+});
 
+return (
+    <Modal
+      as={Form}
+      open={open}
+      size='small'
+      trigger={<Button>Add New Kid </Button>}
+    >
+      <Header icon="save" content="New Kid Info" as="h3" />
+      <Modal.Content>
+        <Form.Input 
+            label="Name" 
+            name="name"
+            type="text" 
+            onChange={handleChange}
+            value={formData.name} />
+        <Form.Input 
+            label="Dietary Restrictions" 
+            name="dietary_restrictions"
+            type="text"
+            onChange={handleChange} 
+            value={formData.dietary_restrictions} />
+      </Modal.Content>
+      <Modal.Actions>
+        {/* <Button type="button" basic color='red' onClick={() => setOpen(false)}>
+          <Icon name='remove' /> Close
+        </Button> */}
+        <Button color='green' type="submit">
+          <Icon name='checkmark' /> Submit
+        </Button>
+      </Modal.Actions>
+    </Modal>
+)
 }
 
 export default NewKid;
