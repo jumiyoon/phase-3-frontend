e660744e5a5b0b6f2271f39453f1f9f22ad43683
Jumi Yoon <jums.code@gmail.com>
2023-04-24 13:08:46 -0700

    create onSubmit event listener

--- src/components/NewKid.js
+++ src/components/NewKid.js
@@ -23,12 +23,30 @@ function NewKid(){
         console.log(e.target.name, ":", e.target.value)
         console.log(formData)
     }
+    
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(e)
+    
+        fetch("http://localhost:9292/kids", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        })
+          .then((r) => r.json())
+          .then((newKid) => {onAddKid(newKid)});
+        //     setSaved(!saved);
+      }
 
     return (
         <Modal
             as={Form}
             open={open}
             size='small'
+            onSubmit={(e)=> handleSubmit(e)} 
+            onClose={() => setOpen(false)}
             onOpen={() => setOpen(true)}
             trigger={<Button>Add New Kid </Button>}
         >
