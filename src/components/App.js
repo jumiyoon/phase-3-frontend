05bf3a8f168c23e1e54b6cd389f849ee45deab0f
Jumi Yoon <jums.code@gmail.com>
2023-04-24 12:30:06 -0700

    Add KidsList component, import to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from "react";
 import Header from "./Header";
+import KidsList from "/KidsList";
 function App() {
+    const [kids, setKids] = useState([]);
 
-useEffect(() => {
-    fetch("http://localhost:9292/kids")
-    .then((res) => res.json())
-    .then((kids) => setKids(kids))
-}, [])
+    useEffect(() => {
+        fetch("http://localhost:9292/kids")
+        .then((res) => res.json())
+        .then((kids) => setKids(kids))
+    }, [])
 
 
 
     return (
          <main className="header-color">
             <Header/>
+            <KidList kids={kids} />
          </main>
     )
 }
---
+++ src/components/KidsList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function KidsList() {
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default KidsList;
+
+
