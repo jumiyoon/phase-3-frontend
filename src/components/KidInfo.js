03e9c803d46dda5ec13543266f9fd692be5ed721
Jumi Yoon <jums.code@gmail.com>
2023-04-24 12:33:33 -0700

    create Parent info button

--- src/components/KidInfo.js
+++ src/components/KidInfo.js
@@ -1,6 +1,14 @@
 import React from "react";
+import { Button } from 'semantic-ui-react';
 
 function KidInfo( { id, name, dietaryRestrictions, parentName, parentPhone, pickupTime, onDeleteKid } ) {
+    const parentInfoButton = <Button size="tiny" basic color='black' onClick = {() => setIsOpen(!isOpen)}>{ isOpen ? "Hide Parent Info" : "View Parent Info" } </Button>
+    const parentInfo = 
+        (<div>
+            <b>👨‍👧‍👦 Name:</b> {parentName} <br/>
+            <b>📱 Phone Number: </b> {parentPhone} <br/>
+            <b>⏰ Pickup Time: </b> {pickupTime}
+        </div>)
 
     return (
         <div>
