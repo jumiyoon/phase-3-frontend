c17739259dd89f0d3da49950eb9b9b9536a486dc
Jumi Yoon <jums.code@gmail.com>
2023-04-24 12:40:49 -0700

    pass down prop to EditRestrictions component

--- src/components/EditRestrictions.js
+++ src/components/EditRestrictions.js
@@ -2,12 +2,13 @@ import React, { useState } from "react";
 import { Modal, Button, Form } from 'semantic-ui-react'
 
 
-function EditRestrictions({ } ) {
+function EditRestrictions({ id } ) {
+    const [restrictions, setRestrictions] = useState("");
 
 
     return (
         <div>
-            
+
         </div>
 
     )
