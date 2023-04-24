ff6e50a52805411b9756b32c1475b9b828573596
Jumi Yoon <jums.code@gmail.com>
2023-04-24 12:41:09 -0700

    create patch request

--- src/components/EditRestrictions.js
+++ src/components/EditRestrictions.js
@@ -5,6 +5,19 @@ import { Modal, Button, Form } from 'semantic-ui-react'
 function EditRestrictions({ id } ) {
     const [restrictions, setRestrictions] = useState("");
 
+    function handleFormSubmit() {
+        fetch(`http://localhost:9292/kids/${id}`, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            dietary_restrictions: restrictions
+          }),
+        })
+          .then((r) => r.json()) 
+    }
+
 
     return (
         <div>
